15f93e2bcfecf86b51bc5fc2f2077a7987e8fd41
VihangaSathsaranieThalangedara <119300736+VihangaST@users.noreply.github.com>
2025-08-31 23:06:35 +0530

    form component added-add new customers

--- frontend/src/pages/Customers.jsx
+++ frontend/src/pages/Customers.jsx
@@ -1,8 +1,25 @@
 import React from "react";
+import AddCustomerForm from "../components/AddCustomerForm";
 function Customers() {
   return (
     <>
-      <h1>customers</h1>
+      <AddCustomerForm />
+      <table className="table table-striped-columns">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Contact No</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+          </tr>
+        </tbody>
+      </table>
     </>
   );
 }
