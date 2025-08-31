49e222a45a49100adcdd5ce0d05e20714dd28f44
VihangaSathsaranieThalangedara <119300736+VihangaST@users.noreply.github.com>
2025-08-31 20:20:18 +0530

    ProductCard component modified to display product details

---
+++ frontend/src/components/ProductCard.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function ProductCard({ product }) {
+  return (
+    <>
+      <div className="card" style={{ width: "14rem" }}>
+        <img src={`${product.image}`} alt={product.name} />{" "}
+        <div className="card-body">
+          <h5 className="card-title">{product.name}</h5>
+          <p className="card-text">{product.weight}</p>
+          <p className="card-text">{product.costPrice}</p>
+          <p className="card-text">{product.sellingPrice}</p>
+          <p className="card-text">{product.markerPrice}</p>
+
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ProductCard;
